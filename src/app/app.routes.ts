93d4e21b4520e00f43079c5e1fa34faa7c506f19
Juan Anaya <jjam2395@gmail.com>
2017-05-04 00:14:29 -0500

    Separando login como componente/mejorando responsive design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,4 +6,4 @@ const APP_ROUTES: Routes = [
 	{path:'**', pathMatch: 'full', redirectTo:''}
 ];
 
-export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES); 
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
