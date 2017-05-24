feaca74483364afc63b8143b7e7d795a75db4dff
Juan Anaya <jjam2395@gmail.com>
2017-05-24 00:53:24 -0500

    cambios para crear curso (form)

--- src/app/components/crear-curso/crear-curso.component.ts
+++ src/app/components/crear-curso/crear-curso.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-
+import { Curso } from '../../interfaces/curso.interface';
+declare var $:any;
 @Component({
   selector: 'app-crear-curso',
   templateUrl: './crear-curso.component.html',
@@ -8,9 +9,28 @@ import { NgForm } from '@angular/forms';
 })
 export class CrearCursoComponent implements OnInit {
 
+  curso:Curso={
+    nombre:"",
+    objetivo:"",
+    descripcion:"",
+    instructores:[]
+  }
   constructor() { }
-
+  hola:any;
   ngOnInit() {
+
+    $('.chips').material_chip();
+
+    $('.chips').on('chip.add', function(e, chip){
+      console.log("chip",chip);
+      this.hola=chip;
+    // you have the added chip here
+      console.log(this.hola);
+    });
+  }
+
+  guardar(){
+    console.log(this.curso);
   }
 
 }
