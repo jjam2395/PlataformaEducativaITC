ad966b25591b7baf35fcd0f1f02ae7f46026dfbb
Juan Anaya <jjam2395@gmail.com>
2017-06-05 09:39:32 -0500

    modificación de inicio-alumno

--- src/app/components/shared/side-nav/side-nav.component.ts
+++ src/app/components/shared/side-nav/side-nav.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../../../services/login.service';
+import {UsuarioAlumnoService} from '../../../services/usuarioAlumno.service';
 import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-side-nav',
@@ -8,11 +10,22 @@ import { Router } from '@angular/router';
   styleUrls: ['./side-nav.component.css']
 })
 export class SideNavComponent implements OnInit {
-	nameUser: String;
+  nameUser: String;
 	materias: Array<String>;
-  constructor(private _ls: LoginService,  private router:Router) {
+  constructor(private _ls: LoginService, private _ua:UsuarioAlumnoService,  private router:Router) {
   	this.nameUser="localStorage.user.displayName";
   	this.materias=["Base de datos","Dispositivos Moviles","Inteligencia Artificial"];
+
+    let userUid=JSON.parse(localStorage.getItem('user')).uid;
+    this._ua.getUser(localStorage.getItem('tipoUserLogeado'),userUid)
+    .subscribe( (user)=>{
+      console.log("Usuario Cargado..");
+      console.log("emm:",(user.cursos));
+      this.nameUser=user.displayName;
+    })
+;
+
+
   }
 
   ngOnInit() {
--- src/environments/firebase.conf.ts
+++ src/environments/firebase.conf.ts
@@ -1,9 +1,9 @@
 export const firebaseConf = {
 	apiKey: "AIzaSyAJWSYEENV38MQFOeqFvYPz-5Et_pSjCOI",
-		authDomain: "plataformaeducativaitc-50026.firebaseapp.com",
-		databaseURL: "https://plataformaeducativaitc-50026.firebaseio.com",
-		projectId: "plataformaeducativaitc-50026",
-		storageBucket: "plataformaeducativaitc-50026.appspot.com",
-		messagingSenderId: "549671738587"
+    authDomain: "plataformaeducativaitc-50026.firebaseapp.com",
+    databaseURL: "https://plataformaeducativaitc-50026.firebaseio.com",
+    projectId: "plataformaeducativaitc-50026",
+    storageBucket: "plataformaeducativaitc-50026.appspot.com",
+    messagingSenderId: "549671738587"
 
 }
